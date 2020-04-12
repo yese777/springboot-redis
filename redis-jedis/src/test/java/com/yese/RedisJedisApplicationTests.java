0f6caf6107a4b585a5525bc6cf5399cd6c906d20
张庆福 <15195988693@163.com>
2020-04-12 19:56:56 +0800

    更新

--- redis-jedis/src/test/java/com/yese/RedisJedisApplicationTests.java
+++ redis-jedis/src/test/java/com/yese/RedisJedisApplicationTests.java
@@ -5,6 +5,9 @@ import com.yese.service.Userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 @SpringBootTest
 class RedisJedisApplicationTests {
@@ -23,4 +26,38 @@ class RedisJedisApplicationTests {
         User user = userservice.getById("2");
         System.out.println(user);
     }
+
+
+
+    @Autowired
+    private JedisPool jedisPool;//Jedis连接池
+
+    /**
+     * 事务的简单演示
+     */
+    @Test
+    void contextLoads() {
+        // 得到 jedis 对象
+        Jedis jedis = jedisPool.getResource();
+
+        // 开启事务
+        Transaction multi = jedis.multi();
+
+        try {
+            multi.set("name1", "zhangsan");
+            multi.set("name2", "lisi");
+            // 模拟抛出异常
+            int i = 1 / 0;
+            // 执行事务
+            multi.exec();
+        } catch (Exception e) {
+            // 失败就放弃事务
+            multi.discard();
+            e.printStackTrace();
+        } finally {
+            // 关闭连接
+            jedis.close();
+        }
+    }
+
 }
--- redis-lettuce/src/main/java/com/yese/config/RedisConfig.java
+++ redis-lettuce/src/main/java/com/yese/config/RedisConfig.java
@@ -17,7 +17,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         // 配置连接工厂
         template.setConnectionFactory(factory);
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
         Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
